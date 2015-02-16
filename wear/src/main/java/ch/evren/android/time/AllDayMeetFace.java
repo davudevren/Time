1fb4416fad90294b99757a14108a10d97bd2b7ec
davudevren <davud_evren@yahoo.de>
2015-02-16 15:56:42 +0100

    Initial Commit

---
+++ wear/src/main/java/ch/evren/android/time/AllDayMeetFace.java
@@ -0,0 +1,24 @@
+package ch.evren.android.time;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class AllDayMeetFace extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_day_meet_face);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
